4f24b56e4abc260dc446d24cffdf15bbd61f88cc
Alex Graden <97418444+agraden@users.noreply.github.com>
2023-09-17 16:34:23 -0400

    Add project files.

---
+++ BlobApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+string connectionString = "";
+string containerName = "scripts1";
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+// create container and specify access type
+await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
+
+Console.WriteLine("Container created");

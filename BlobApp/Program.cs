f141ee686ac4f576be92919811506839209b14bb
Alex Graden <97418444+agraden@users.noreply.github.com>
2023-09-17 21:05:18 -0400

    remove obsolete code

--- BlobApp/Program.cs
+++ BlobApp/Program.cs
@@ -6,12 +6,7 @@ string containerName = "scripts1";
 string blobName = "scripts.sql";
 string filePath = "C:\\temp\\script.sql";
 
-// BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString, containerName);
-
-// create container and specify access type
-// await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
-
 
 // establish client to create blob
 BlobClient blobClient = blobServiceClient.GetBlobClient(blobName);
